ad8f551cac20ba45f3362552afcb41dee714d9c4
Jovan Stoicev <jovan.stoicev55@gmail.com>
2026-07-08 22:08:33 +0200

    Innitial commit

---
+++ src/test/java/com/billing/minibilling/MinibillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.minibilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
